219a3303a33635867342207bee5bff0ea74a02b9
Juan Carlos <104027144+Juanka08@users.noreply.github.com>
2023-06-14 16:58:56 -0300

    Update app.py

--- source/app.py
+++ source/app.py
@@ -7,6 +7,9 @@ def index():
     variable = "Hello, world!"
     variable1 = variable
     variable2 = variable1
+    variable2 = variable1
+    variable2 = variable1
+    variable2 = variable1
     return variable2
 
 if __name__ == "__main__":
