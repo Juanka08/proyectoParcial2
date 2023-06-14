4c966d50aa6dc2a73b999d39742ac668b9dd2e8d
Juanka08 <juancito.jcnc@gmail.com>
2023-06-14 19:18:50 -0300

    sdasasf

--- source/app.py
+++ source/app.py
@@ -1,19 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__)
-variable = " "
-variable1 = variable
-variable2 = variable1
-    
+
 @app.route("/")
 def index():
-    variable2 = variable1
-    variable2 = variable1
-    variable2 = variable1
-    return variable2
-
-def indexvacio():
-    return
+    return "Hello, world!"
 
 if __name__ == "__main__":
     app.run()
